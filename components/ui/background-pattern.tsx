2f2c44cf1f01e7bfee21447fdd0964d53d000f70
Jake Bagsit <icandrew08@gmail.com>
2025-02-25 00:05:54 +0800

    refactor: remove galaxy effect from background pattern

--- components/ui/background-pattern.tsx
+++ components/ui/background-pattern.tsx
@@ -134,13 +134,6 @@ const Background = forwardRef<HTMLDivElement, BackgroundProps>(
 
         return (
             <>
-                {darkGradient && (
-                    <div className="pointer-events-none fixed inset-0">
-                        <div className="absolute inset-0 bg-gradient-to-b from-background via-background/90 to-background" />
-                        <div className="absolute right-0 top-0 h-[500px] w-[500px] bg-blue-500/10 blur-[100px]" />
-                        <div className="absolute bottom-0 left-0 h-[500px] w-[500px] bg-purple-500/10 blur-[100px]" />
-                    </div>
-                )}
                 {gradient.display && (
                     <div
                         ref={backgroundRef}
